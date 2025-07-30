0f6de7e95e4c431a52d8044e129a20ec79b6fc91
pramadito <pramadito@gmail.com>
2025-07-30 11:27:30 +0700

    feat: add forgot password (#2)

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,7 +1,9 @@
 import { ApiError } from "../../utils/api-error";
 import { JwtService } from "../jwt/jwt.service";
+import { MailService } from "../mail/mail.service";
 import { PasswordService } from "../password/password.service";
 import { PrismaService } from "../prisma/prisma.service";
+import { ForgotPasswordDTO } from "./dto/forgot-password.dto";
 import { LoginDTO } from "./dto/login.dto";
 import { RegisterDTO } from "./dto/register.dto";
 
@@ -9,10 +11,12 @@ export class AuthService {
   private prisma: PrismaService;
   private passwordService: PasswordService;
   private jwtService: JwtService;
+  private mailService: MailService;
   constructor() {
     this.prisma = new PrismaService();
     this.passwordService = new PasswordService();
     this.jwtService = new JwtService();
+    this.mailService = new MailService();
   }
   register = async (body: RegisterDTO) => {
     const user = await this.prisma.user.findFirst({
@@ -24,7 +28,7 @@ export class AuthService {
       throw new ApiError("User already exists", 400);
     }
 
-     const hashedPassword = await this.passwordService.hashPassword(
+    const hashedPassword = await this.passwordService.hashPassword(
       body.password
     );
 
@@ -43,7 +47,7 @@ export class AuthService {
       where: {
         email: body.email,
       },
-    })
+    });
     if (!user) {
       throw new ApiError("User not found", 400);
     }
@@ -57,16 +61,49 @@ export class AuthService {
       throw new ApiError("Invalid password", 400);
     }
 
-    const payload = {id: user.id}
+    const payload = { id: user.id };
 
     const accessToken = await this.jwtService.generateToken(
       payload,
       process.env.JWT_SECRET!,
-      {expiresIn: "2h"}
+      { expiresIn: "2h" }
     );
-    
-    const {password, ...userWithoutPassword} = user;
 
-    return {...userWithoutPassword, accessToken};
+    const { password, ...userWithoutPassword } = user;
+
+    return { ...userWithoutPassword, accessToken };
+  };
+
+  forgotPassword = async (body: ForgotPasswordDTO) => {
+    const user = await this.prisma.user.findFirst({
+      where: { email: body.email },
+    });
+
+    if (!user) {
+      throw new ApiError("Invalid email address", 400);
+    }
+
+    const payload = { id: user.id };
+
+    const token = this.jwtService.generateToken(
+      payload,
+      process.env.JWT_SECRET_RESET!,
+      { expiresIn: "15m" }
+    );
+
+    const resetLink = `http://localhost:3000/reset-password/${token}`;
+    await this.mailService.sendMail(
+      body.email,
+      "Reset Password",
+      "forgot-password",
+      {
+        name: user.name,
+        resetLink: resetLink,
+        expireMinutes: "15",
+        year: new Date().getFullYear(),
+      }
+    );
+
+    return { message: "send email successfully" };
   };
 }
